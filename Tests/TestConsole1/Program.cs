dad6b971c3d475a885d16427e4112986ec668924
Yan-Hua Wang (彥) <yanhua1012@gmail.com>
2019-08-28 13:56:40 +0800

    加入測試程式

---
+++ Tests/TestConsole1/Program.cs
@@ -0,0 +1,59 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Protocol;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+            Console.ReadKey();
+
+        }
+
+        private static async Task Start()
+        {
+            var managedClient = new MqttFactory().CreateManagedMqttClient();
+
+            var options = new ManagedMqttClientOptions
+            {
+                ClientOptions = new MqttClientOptions
+                {
+                    ClientId = "MQTTnetManagedClientTest",
+                    Credentials = new MqttClientCredentials
+                    {
+                        Username = string.Empty,
+                        Password = new byte[] { }
+                    },
+
+                    ChannelOptions = new MqttClientTcpOptions
+                    {
+                        Server = "localhost"
+                    }
+                },
+
+                AutoReconnectDelay = TimeSpan.FromSeconds(1),
+                //Storage = ms
+            };
+
+            managedClient.UseApplicationMessageReceivedHandler(HandleReceivedApplicationMessage);
+            await managedClient.StartAsync(options);
+            await managedClient.SubscribeAsync("home/garden/fountain", MqttQualityOfServiceLevel.AtLeastOnce);
+
+        }
+
+        private static async Task HandleReceivedApplicationMessage(MqttApplicationMessageReceivedEventArgs eventArgs)
+        {
+            string payload = Encoding.Default.GetString(eventArgs.ApplicationMessage.Payload);
+            Console.WriteLine($"received from {eventArgs.ClientId}: {payload}");
+            await Task.CompletedTask;
+        }
+    }
+}
